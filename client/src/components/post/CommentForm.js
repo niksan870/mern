f2bffc45dd1ea47eafdbd04ad471b38cdf262039
Nikola Pashov <niksan870@abv.bg>
2019-04-03 10:30:56 +0300

    Prepared for deploy

--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -2,31 +2,19 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
-import { addPost } from "../../actions/postActions";
+import { addComment } from "../../actions/postActions";
 
-class PostForm extends Component {
+class CommentForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       text: "",
       errors: {}
     };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
-
-  onSubmit = e => {
-    e.preventDefault();
-
-    const { user } = this.props.auth;
-
-    const newPost = {
-      text: this.state.text,
-      name: user.name,
-      avatar: user.avatar
-    };
-
-    this.props.addPost(newPost);
-    this.setState({ text: "" });
-  };
 
   componentWillReceiveProps(newProps) {
     if (newProps.errors) {
@@ -34,9 +22,25 @@ class PostForm extends Component {
     }
   }
 
-  onChange = e => {
+  onSubmit(e) {
+    e.preventDefault();
+
+    const { user } = this.props.auth;
+    const { postId } = this.props;
+
+    const newComment = {
+      text: this.state.text,
+      name: user.name,
+      avatar: user.avatar
+    };
+
+    this.props.addComment(postId, newComment);
+    this.setState({ text: "" });
+  }
+
+  onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
-  };
+  }
 
   render() {
     const { errors } = this.state;
@@ -44,12 +48,14 @@ class PostForm extends Component {
     return (
       <div className="post-form mb-3">
         <div className="card card-info">
-          <div className="card-header bg-info text-white">Say Somthing...</div>
+          <div className="card-header bg-info text-white">
+            Make a comment...
+          </div>
           <div className="card-body">
             <form onSubmit={this.onSubmit}>
               <div className="form-group">
                 <TextAreaFieldGroup
-                  placeholder="Create a post"
+                  placeholder="Reply to post"
                   name="text"
                   value={this.state.text}
                   onChange={this.onChange}
@@ -67,18 +73,19 @@ class PostForm extends Component {
   }
 }
 
-PostForm.propTypes = {
-  addPost: PropTypes.func.isRequired,
+CommentForm.propTypes = {
+  addComment: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
+  postId: PropTypes.string.isRequired,
   errors: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
-  errors: state.errors,
-  auth: state.auth
+  auth: state.auth,
+  errors: state.errors
 });
 
 export default connect(
   mapStateToProps,
-  { addPost }
-)(PostForm);
+  { addComment }
+)(CommentForm);
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb://niksan870:niksan870@ds119606.mlab.com:19606/dev-connector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
